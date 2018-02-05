0765dad00fb30be1296d5e1ea6fb8d921f911842
dz <dengzheng80s@163.com>
2018-02-05 17:27:17 +0800

    IDEA整合Git

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.jd.test;
+
+/**
+ * 你好呀，此处应该设置成全局和工程的编码格式都为UTF-8
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("-----------------");
+    }
+
+}
